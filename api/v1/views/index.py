b8e721d4cb19440ef2eb0695d7e744b685995a6a
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-10-24 00:13:22 +0300

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,59 +1,24 @@
 #!/usr/bin/python3
-
-""" index module showing the status endpoint """
-
-import json
-
+""" Index script """
+from api.v1.views import app_views
+from flask import jsonify
 from models import storage
 
-from models.state import State
 
-from models.amenity import Amenity
-
-from models.user import User
-
-from models.city import City
-
-from models.review import Review
-
-from models.place import Place
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of the API """
+    return jsonify({"status": "OK"})
 
 
-
-from api.v1.views import app_views
-
-
-
-
-
-@app_views.route('/status')
-
-def show_status():
-
-        return json.dumps({"status": "OK"})
-
-
-
-
-
-    @app_views.route('/api/v1/stats')
-
-    def show_count():
-
-            new_dict = {
-
-                            "amenities": storage.count(Amenity),
-
-                                    "cities": storage.count(City),
-
-                                            "places": storage.count(Place),
-
-                                                    "reviews": storage.count(Review),
-
-                                                            "states": storage.count(State),
-
-                                                                    "users": storage.count(User)
-
-                                                                        }
-
-                return json.dumps(new_dict)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns stats of objects """
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
