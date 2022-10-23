92c9af769fe8e19098b4ee478f74df4b7d17a783
Nellymaina <99280364+Nellymaina@users.noreply.github.com>
2022-10-24 00:33:22 +0300

    task 3

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,67 +1,78 @@
 #!/usr/bin/python3
-
-"""
-
-Sets up the Flask application
-
-"""
-
-
-
-
-
-from os import getenv
-
-from flask import Flask, make_response, jsonify
-
-from flask_cors import CORS
-
+""" Amenities view """
 from api.v1.views import app_views
-
+from flask import Flask, jsonify, abort, make_response, request
 from models import storage
+from models.amenity import Amenity
 
 
+@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
+def all_amenities():
+    """ Returns a list of all the amenities """
+    amenities = []
+    for amenity in storage.all("Amenity").values():
+        amenities.append(amenity.to_dict())
 
-app = Flask(__name__)
-
-CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
-
-app.register_blueprint(app_views)
-
-
-
-
-
-@app.errorhandler(404)
-
-def page_not_found(error):
-
-        """ Returns JSON error response """
-
-            return make_response(jsonify({'error': 'Not found'}), 404)
+    return jsonify(amenities)
 
 
+@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+def amenity(amenity_id):
+    """ Returns an amenity by id """
+    amenity = storage.get("Amenity", amenity_id)
+
+    if not amenity:
+        abort(404)
+
+    return jsonify(amenity.to_dict())
 
 
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_amenity(amenity_id):
+    """ Deletes an amenity from storage based on id """
+    amenity = storage.get("Amenity", amenity_id)
 
-        @app.teardown_appcontext
+    if not amenity:
+        abort(404)
 
-        def teardown(self):
+    storage.delete(amenity)
+    storage.save()
 
-                """ Closes storage session for SQLAlchemy database """
-
-                    storage.close()
-
-
+    return make_response(jsonify({}), 200)
 
 
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def add_amenity():
+    """ Creates a new amenity """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
 
-                    if __name__ == '__main__':
+    if not request.get_json().get('name'):
+        abort(400, description="Missing name")
 
-                            """ Main function for the hosts and ports """
+    amenity = Amenity()
+    amenity.name = request.get_json()['name']
+    amenity.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
 
-                                api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
 
-                                    api_port = getenv('HBNB_API_PORT', default=5000)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+def update_amenity(amenity_id):
+    """ Updates an amenity specified by id """
+    amenity = storage.get("Amenity", amenity_id)
 
-                                        app.run(host=api_host, port=int(api_port), threaded=True)
+    if not amenity:
+        abort(404)
+
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    for key, value in request.get_json().items():
+        if key == "id" or key == "created_at" or key == "updated_at":
+            continue
+        else:
+            setattr(amenity, key, value)
+
+    storage.save()
+
+    return make_response(jsonify(amenity.to_dict()), 200)
