b3d686c572b27fcf32e3b03258aa367117ae5f21
Terry <terrybmochire@gmail.com>
2022-10-24 15:16:40 +0300

    Update get and count methods.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -76,14 +76,31 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """Retrieve one object"""
-        if cls in classes:
-            key = cls + '.' + id
-            return self.all(cls).get(key)
+        """
+        Returns the object based on the class name and its ID, or
+        None if not found
+        """
+        if cls not in classes.values():
+            return None
+
+        all_cls = models.storage.all(cls)
+        for value in all_cls.values():
+            if value.id == id:
+                return value
+
         return None
 
     def count(self, cls=None):
-        """Count the number of objects in storage"""
-        if cls in classes:
-            return len(self.all(cls))
-        return len(self.all())
+        """
+        count the number of objects in storage
+        """
+        all_class = classes.values()
+
+        if not cls:
+            count = 0
+            for clas in all_class:
+                count += len(models.storage.all(clas).values())
+        else:
+            count = len(models.storage.all(cls).values())
+
+        return count
